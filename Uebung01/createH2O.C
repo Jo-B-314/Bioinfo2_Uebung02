64ba9d0e35594edafa8b635a57ca37caec142868
Lisa-Marie Rolli <s8liroll@stud.uni-saarland.de>
2021-04-26 16:23:39 +0200

    argparser added

--- Uebung01/createH2O.C
+++ Uebung01/createH2O.C
@@ -8,7 +8,85 @@
 using namespace std;
 using namespace BALL;
 
+
+/**
+ * our main can be called ./getSequence -a angle -deg/-rad
+ * the last flag specifies whethter your angle is in deg or rad
+ * all the parameters are not optional
+ * using -h or --help you'll get help
+ **/
 int main(int argc, char* argv[]) {
+	string h = "-h";
+	string help = "--help";
+	for (int i = 0; i < argc; i++) {
+		if (h.compare(argv[i]) == 0 || help.compare(argv[i]) == 0) {
+			//-h or --help found
+			cout << "Help window:\n-a[angle] : required, angle (H-O-H), you have to specify whether it is in deg or rad\n";
+			cout << "-deg/-rad: required, specifies whether your angle is in deg or rad\n";
+			cout << "-f/--file[PDB-FILE]: required, .pdb file where you want to store you molecule\n ";
+			cout << "-h/--help: open this help window\n";
+			return 0;
+		}
+	}
+	if (argc != 6) {
+		cerr << "Wrong number of arguments. Use -h or --help to open the help menu.\n";
+		return 1;
+	}
+	double angle;
+	bool deg;
+	string a = "-a";
+	string degstr = "-deg";
+	string radstr = "-rad";
+	string anglestr;
+	string str;
+	string file_name;
+	string f = "-f";
+	string file = "--file";
+	for (int i = 0; i < argc; i++) {
+		if (!a.compare(argv[i])) {
+			if (i+1 >= argc) {
+				cerr << "No angle given. Use -h or --help to open the help menu.\n";
+				return 1;
+			}
+			if (!anglestr.empty()) {
+				cerr << "More than one angle given. Use -h or --help to open the help menu.\n";
+				return 1;
+			}
+			anglestr = argv[i+1];
+			angle = stod(anglestr);
+		} 
+		if (!degstr.compare(argv[i])) {
+			if (!str.empty()) {
+				cerr << "More than one angle specification given. Use -h or --help to open the help menu.\n";
+				return 1;
+			}
+			str = argv[i];
+			deg = true;
+		}
+		if (!radstr.compare(argv[i])) {
+			if (!str.empty()) {
+				cerr << "More than one angle specification given. Use -h or --help to open the help menu.\n";
+				return 1;
+			}
+			str = argv[i];
+			deg = false;
+		}
+		if (!f.compare(argv[i]) || !file.compare(argv[i])) {
+			if (!file_name.empty()) {
+				cerr << "More than one output file given. Use -h or --help to open the help menu.\n";
+				return 1;
+			}
+			if (i+1 >= argc) {
+				cerr << "Flag but no output file given. Use -h or --help to open the help menu.\n";
+				return 1;
+			}
+			file_name = argv[i+1];
+		}
+	}
+	if (str.empty() || anglestr.empty() || file_name.empty()) {
+		cerr << "Not all required arguments given. Use -h or --help to open the help menu.\n";
+		return 1;
+	}
 	//b)
 	System s;
 	Molecule mol;
